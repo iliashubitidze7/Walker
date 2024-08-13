2345e18048f84a35ad9f9f8e1cb878035af6c6e6
iliashubiitdze <shubitidzeilia7@gmail.com>
2024-08-13 21:00:52 +0400

    Add project files.

---
+++ WalksAPI/Controllers/RegionsController.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using System.Security.Cryptography.Xml;
+using WalksAPI.Data;
+using WalksAPI.Models.Domain;
+using WalksAPI.Models.DTO;
+
+namespace WalksAPI.Controllers
+{
+    //https//localhost:1234/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public RegionsController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regionsDomain  = _dbContext.Regions.ToList();
+
+            var regionsDto = new List<RegionDto>();
+
+            foreach (var region in regionsDomain) 
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = region.Id,
+                    Name = region.Name,
+                    Code = region.Code,
+                    RegionImageUrl = region.RegionImageUrl,
+                });
+            }
+            return Ok(regionsDto);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id) {
+
+            var region = _dbContext.Regions.FirstOrDefault(u => u.Id == id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            var regionsDto = new RegionDto
+            {
+                Id = region.Id,
+                Name = region.Name,
+                Code = region.Code,
+                RegionImageUrl = region.RegionImageUrl,
+            };
+
+            return Ok(regionsDto);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+
+            var regionDomainModel = new Region
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl,
+            };
+
+            _dbContext.Regions.Add(regionDomainModel);
+            _dbContext.SaveChanges();
+
+            var regionDto = new RegionDto
+            {
+                Id = regionDomainModel.Id,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl,
+                Code = regionDomainModel.Code,
+            };
+
+            return CreatedAtAction(nameof(GetById), new {id = regionDto.Id}, regionDto);
+        }
+
+        //update Region
+        //put : //https//localhost:1234/api/regions/{id}
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public IActionResult Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto updateRegionRequestDto)
+        {
+            var regionDomain = _dbContext.Regions.FirstOrDefault(u => u.Id == id);
+
+            if (regionDomain == null) {
+                return NotFound();
+            }
+
+            regionDomain.Code = updateRegionRequestDto.Code;
+            regionDomain.Name = updateRegionRequestDto.Name;
+            regionDomain.RegionImageUrl = updateRegionRequestDto.RegionImageUrl;
+
+            _dbContext.SaveChanges();
+
+            var regionDto = new Region
+            {
+                Id = regionDomain.Id,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl,
+                Code = regionDomain.Code,
+            };
+
+            return Ok(regionDto);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public IActionResult Delete([FromRoute] Guid id) { 
+            var regionDomain = _dbContext.Regions.FirstOrDefault(u => u.Id == id);
+
+            if (regionDomain == null) {
+                return NotFound();            
+            }
+
+            //delete 
+            _dbContext.Regions.Remove(regionDomain);
+            _dbContext.SaveChanges();
+
+            //return deleted region back
+
+
+            return Ok();
+
+        }
+    }
+}
---
+++ WalksAPI/Models/Domain/Difficulty.cs
@@ -0,0 +1,9 @@
+﻿namespace WalksAPI.Models.Domain
+{
+    public class Difficulty
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
